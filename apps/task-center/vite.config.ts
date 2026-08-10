d20446114aab9e398d600a4126f9cdac9944dc91
ER1C-6832 <shangdomo2-c@my.cityu.edu.hk>
2026-08-10 15:52:17 +0800

    fix app main ui

--- apps/task-center/vite.config.ts
+++ apps/task-center/vite.config.ts
@@ -1,12 +1,14 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === "build" ? "/app/" : "/",
   server: {
+    host: "127.0.0.1",
     port: 5173
   },
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   }
-});
+}));
