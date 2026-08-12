ddbce01a7fb00c5f43ccd00b168a6a5933e1d2dc
Er1c <msd342812104@gmail.com>
2026-08-12 09:37:33 +0800

    style: load stage C extension styles

--- apps/extension/src/main.tsx
+++ apps/extension/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './stage-b.css';
+import './stage-c.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
